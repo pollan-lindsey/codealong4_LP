d898d89513d15d8a16dcf0feb7ca22f69f53c803
LINDSEY POLLAN <LPOLLAN@LIVE.COM>
2022-08-12 21:24:07 -0500

    class stuff

--- form-warmup/app.js
+++ form-warmup/app.js
@@ -1,12 +1,28 @@
 // Back to basics -- Forms in HTML
-
+//1. submit a console.log('submitted')
+//2. update our h2 on user input
 
 
 // 1. DOM node (Form, input, etc)
+let myForm = document.getElementById('myForm');
+let userName = document.getElementById('name');
+let header = document.getElementById('nameHeader');
 
-// 2.addEventListener( )
+// 2.handleChange()
+let handleChange = (event) => {
+  let updatedName = event.target.value; //gets whatever the user has entered
+  header.innerText = `Welcome ${updatedName}`;
+};
 
-// 3.HandleChange()
+// handleSubmit('click')
+let handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submitted');
+}
+
+// 3.addEventListener( )
+myForm.addEventListener('submit', handleSubmit)
+userName.addEventListener('input', handleChange); //(what it's listening for, how it will react to change)
 
 // 4.event.preventDefault( )
 
